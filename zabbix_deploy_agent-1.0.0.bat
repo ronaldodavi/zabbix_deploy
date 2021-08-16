bb697a7d70003055915b29d409c9b3eb9c8465f8
ronaldodavi <ronaldodavi@gmail.com>
2021-08-16 12:24:42 -0300

    Update zabbix_deploy_agent-1.0.0.bat
    Modificações de URL e ajuste de erros

--- zabbix_deploy_agent-1.0.0.bat
+++ zabbix_deploy_agent-1.0.0.bat
@@ -1,24 +1,27 @@
 @echo off
+#Necessario para instalar o serviço do zabbix alterar dominio ou colocar usuario com privilegios de administrador.
 runas /user:dominio\administrator
-REM #MODULO 1#
+# se a pasta Existir nao vai fazer nada#
 IF EXIST "c:\Zabbix\zabbix_agentd.exe" (exit) ELSE (echo "Iniciando a instalacao do Zabbix Agent")
-REM #
-REM # Exibe mensagem na tela#
+
+# Exibe mensagem na tela#
 echo Copiando Arquivos
 
-REM #MODULO 2 - Criar o diretório na máquina#
+#Cria diretoria na unidade C:#
 mkdir c:\zabbix
 echo Pasta zabbix criada no C:
 
-REM #MODULO 3 - Realiza a cópia dos binários para o sistema#
+REM #Realiza Download direto do github dos binários para o sistema#
 
 bitsadmin /transfer zagt /priority high https://raw.githubusercontent.com/ronaldodavi/zabbix_deploy/main/agent-v4-lts/zabbix_agentd.exe c:\zabbix\zabbix_agentd.exe
 bitsadmin /transfer zget /priority high https://raw.githubusercontent.com/ronaldodavi/zabbix_deploy/main/agent-v4-lts/zabbix_get.exe c:\zabbix\zabbix_get.exe
 bitsadmin /transfer zgsed /priority high https://raw.githubusercontent.com/ronaldodavi/zabbix_deploy/main/agent-v4-lts/zabbix_sender.exe c:\zabbix\zabbix_sender.exe
-bitsadmin /transfer zconf /priority high https://raw.githubusercontent.com/ronaldodavi/zabbix_deploy/main/zabbix_agentd.conf c:\zabbix\zabbix_agentd.conf
+bitsadmin /transfer zconf /priority high https://raw.githubusercontent.com/ronaldodavi/zabbix_deploy/main/agent-v4-lts/zabbix_agentd.conf c:\zabbix\zabbix_agentd.conf
+bitsadmin /transfer zdll /priority high https://raw.githubusercontent.com/ronaldodavi/zabbix_deploy/main/agent-v4-lts/dev/zabbix_sender.dll c:\zabbix\dev\zabbix_sender.dll
 
-REM #MODULO 4 *PUNK* - Cria arquivo de configuração e define nome do servidor #
+REM #Cria arquivo de configuração e Salvando nome do servidor #
 echo Criando Arquivos de configuracao
+# Alterar para o IP do servidor zabbix ou proxy zabbix
 echo Server=192.168.0.254 > c:\zabbix\zabbix_agentd.conf
 echo Hostname=%COMPUTERNAME% >> c:\zabbix\zabbix_agentd.conf
 echo StartAgents=5 >> c:\zabbix\zabbix_agentd.conf
@@ -27,7 +30,7 @@ echo LogFile=c:\zabbix\zabbix_agentd.log >> c:\zabbix\zabbix_agentd.conf
 echo Timeout=5 >> c:\zabbix\zabbix_agentd.conf
 echo EnableRemoteCommands=1 >> C:\zabbix\zabbix_agentd.conf
 
-REM #MODULO 4 - Ajustes Finais #
+# Instalando Serviços e Gerando LOGs #
 echo Instalando o Serviço
 C:\zabbix\zabbix_agentd.exe -i -c C:\zabbix\zabbix_agentd.conf
 C:\zabbix\zabbix_agentd.exe -s -c C:\zabbix\zabbix_agentd.conf
