76b49a839e2dd4e009e560fe7a43d79d7808a0b7
ronaldodavi <ronaldodavi@gmail.com>
2021-08-18 10:18:48 -0300

    Update zabbix_deploy_agent-v5.bat

--- zabbix_deploy_agent-v5.bat
+++ zabbix_deploy_agent-v5.bat
@@ -25,11 +25,9 @@ echo Timeout=5 >> c:\zabbix\zabbix_agentd.conf
 echo EnableRemoteCommands=1 >> C:\zabbix\zabbix_agentd.conf
 echo HostMetadata=windows >> C:\zabbix\zabbix_agentd.conf
 
-echo Instalando Serviços e Gerando LOGs 
-
 C:\zabbix\zabbix_agentd.exe -i -c C:\zabbix\zabbix_agentd.conf
 C:\zabbix\zabbix_agentd.exe -s -c C:\zabbix\zabbix_agentd.conf
 net start "zabbix agent"
 net restart "zabbix agent"
-echo As configuracoes para o servidor %COMPUTERNAME% foram criadas em %date% as %time%
-echo As configuracoes para o servidor %COMPUTERNAME% foram criadas em %date% as %time% > c:\zabbix\inst_agent.log
+echo As configuracoes para o servidor >> c:\zabbix\inst_agent.log
+echo %COMPUTERNAME% foram criadas em %date% as %time% >> c:\zabbix\inst_agent.log
