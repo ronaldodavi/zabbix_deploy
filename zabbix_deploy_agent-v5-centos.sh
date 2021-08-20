02ce79ec4a29086db205d678dad5ec2543dd1d91
ronaldodavi <ronaldodavi@gmail.com>
2021-08-20 14:56:43 -0300

    Update zabbix_deploy_agent-v5-centos.sh

--- zabbix_deploy_agent-v5-centos.sh
+++ zabbix_deploy_agent-v5-centos.sh
@@ -2,17 +2,17 @@
 #Para Centos e RockLinux
 #Remocao zabbix antigo se houver
 
-yum install wget curl -y
-yum -y remove zabbix*
 
+yum -y remove zabbix*
+wait
 #Instalacao do repositorio
 
 rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm
-
+wait
 #Instalacao do agente zabbix
 
 yum install zabbix-agent -y
-
+wait
 #Backup do arquivo original
 
 mv /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.orig
@@ -35,6 +35,6 @@ HostMetadata=linux
 " >> /etc/zabbix/zabbix_agentd.conf
 
 #Ativacao do agente
-systemctl enable zabbix-agent && systemctl start zabbix-agent
+systemctl enable zabbix-agent && systemctl start zabbix-agent && systemctl status zabbix-agent
 #Liberand porta do agente
 iptables -A INPUT -p tcp -s 192.168.0.254 --dport 10050 -m state --state NEW,ESTABLISHED -j ACCEPT
