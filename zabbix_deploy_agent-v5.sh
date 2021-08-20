8db6bdc1d4120ecedd6ccab12eefe60e98d94065
ronaldodavi <ronaldodavi@gmail.com>
2021-08-20 14:16:14 -0300

    Create zabbix_deploy_agent-v5.sh

---
+++ zabbix_deploy_agent-v5.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Para Centos e RockLinux
+#Remocao zabbix antigo se houver
+
+yum install wget curl -y
+yum -y remove zabbix*
+
+#Instalacao do repositorio
+
+rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm
+
+#Instalacao do agente zabbix
+
+yum install zabbix-agent -y
+
+#Backup do arquivo original
+
+mv /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.orig
+
+#Criacao configucao do arquivo:
+#Alterar IP do server e serverActive
+echo "
+
+Server=192.168.0.254
+ServerActive=192.168.0.254
+Hostname=$(hostname)
+StartAgents=5
+DebugLevel=3
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+LogFile=/var/log/zabbix/zabbix_agentd.log
+Timeout=5
+EnableRemoteCommands=1
+HostMetadata=linux
+
+" >> /etc/zabbix/zabbix_agentd.conf
+
+#Ativacao do agente
+systemctl enable zabbix-agent && systemctl start zabbix-agent
+#Liberand porta do agente
+iptables -A INPUT -p tcp -s 192.168.0.254 --dport 10050 -m state --state NEW,ESTABLISHED -j ACCEPT
